f3aee0bed117a05fa6cf607b6f3ffe9f0bf411cf
Lev Kadatscky <levkadatscky@gmail.com>
2023-10-30 15:27:22 +0200

    added commentaries for functions

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def index():
         max_distance = calculate_max_distance(max_area, angle_width, angle_height)
         return jsonify({
             'angle_width': degrees(angle_width),
-            'angle_height':  degrees(angle_height),
+            'angle_height': degrees(angle_height),
             'max_area': max_area,
             'max_distance': max_distance,
         })
@@ -75,16 +75,45 @@ def module_3():
 
 
 def calculate_divergence_angle(size, distance):
+    """
+    Calculates divergence angle
+    :param size: size of plume
+    :param distance: distance
+    :type size: float
+    :type distance: float
+    :return: returns angle value
+    :rtype: float
+    """
     angle = 2 * atan(size / (2 * distance))
     return angle
 
 
 def calculate_max_area(sensitivity, power):
+    """
+    Calculates max area of ellipse
+    :param sensitivity: sensitivity of receiver
+    :param power: power of receiver
+    :type sensitivity: float
+    :type power: float
+    :return: returns a max area
+    :rtype float
+    """
     max_area = power / sensitivity
     return max_area
 
 
 def calculate_max_distance(max_area, angle_width, angle_height):
+    """
+    calculate max guaranteed distance of data transfer
+    :param max_area: value calculated in calculate_max_area()
+    :param angle_width: value of width angle
+    :param angle_height: value of height angle
+    :type max_area: float
+    :type angle_width: float
+    :type angle_height: float
+    :return: returns a value of max distance
+    :rtype: float
+    """
     max_angle = max(angle_width, angle_height)
     min_angle = min(angle_width, angle_height)
     coefficient = min_angle / max_angle
@@ -93,11 +122,29 @@ def calculate_max_distance(max_area, angle_width, angle_height):
 
 
 def calculate_size(angle, distance):
+    """
+    Calculates the size of plume
+    :param angle: value of angle
+    :param distance: value of distance
+    :type angle: float
+    :type distance: float
+    :return: returns a plume size
+    :rtype: float
+    """
     size = 2 * distance * tan(angle / 2)
     return size
 
 
 def calculate_distance(size, angle):
+    """
+    Calculates the minimal effective distance
+    :param size: size of the plume
+    :param angle: angle value
+    :type size: float
+    :type angle: float
+    :return: returns the value of the minimal effective distance
+    :rtype: float
+    """
     distance = size / (2 * tan(angle / 2))
     return distance
 
